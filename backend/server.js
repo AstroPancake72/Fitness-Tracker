33c6c2237931d9cd61c3eb58d867c97a64dd3427
Riya <rpatil1@g.ucla.edu>
2026-05-08 15:54:25 -0700

    Setup MongoDB for backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Backend running");
